4891653e445d92383823d204ff9af96d231db02e
JEONG AH SHIN <jeongahri@JEONGui-MacBook-Pro-2.local>
2017-12-09 01:41:07 +0900

    setting for displaying tables

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import pandas as pd
+from pandas import Series, DataFrame
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  return 'Lottery Analysis'
+def show_tables():
+    rawData = pd.read_csv('lottery.txt', sep='\t')
+    rawData.set_index(['round'],inplace = True)
+
+    return rawData.to_html(classes='lottery_result')
 
 if __name__ == '__main__':
-  app.run()
+    app.debug = True
+    app.run()
